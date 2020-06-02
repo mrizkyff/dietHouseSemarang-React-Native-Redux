07adb5fe95b83fb7b58f06b5d8d820aad78812de
mrizkyff <muhamadrizkyff@gmail.com>
2020-06-03 05:55:43 +0700

    done login form

--- screens/login/index.js
+++ screens/login/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet, Image } from 'react-native'
+import { Input, Item, Icon, Button } from "native-base"
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 // KOMPONEN
 // logo
@@ -7,18 +9,45 @@ const Logo = () => {
     return (
         <View style={styles.viewLogo}>
             <Image source={require('./logo_dhs.png')} style={styles.pictLogo} />
-            <View style={{flexDirection: 'column'}}>
+            <View style={{ flexDirection: 'column' }}>
                 <Text style={{ marginLeft: 10, fontSize: 25, color: 'white' }}>Diet House Semarang</Text>
-                <Text style={{ marginLeft: 10, fontSize: 15, color: 'white' }}>Artisan Food</Text>
+                <Text style={{ marginLeft: 10, fontSize: 20, color: 'white' }}>Artisan Food</Text>
             </View>
         </View>
+    )
+}
+
+// form
+const FormLogin = () => {
+    return (
+        <View style={styles.FormLogin}>
+            {/* <Text>Form Login</Text> */}
+            <Item regular style={{ borderColor: '#696969', backgroundColor: '#808080', height: 50 }}>
+                <Input placeholder='Username' placeholderTextColor='#EEEEEE' style={{ color: '#EEEEEE', fontSize: 20 }} />
+                <Icon type="FontAwesome" name="user" style={{ color: '#EEEEEE', fontSize: 20 }} />
+            </Item>
+            <Item regular style={{ borderColor: '#696969', backgroundColor: '#808080', height: 50, marginTop: 20 }}>
+                <Input placeholder='Password' secureTextEntry={true} placeholderTextColor='#EEEEEE' style={{ color: '#EEEEEE', fontSize: 20 }} />
+                <Icon type="FontAwesome" name="key" style={{ color: '#EEEEEE', fontSize: 20 }} />
+            </Item>
+            <TouchableOpacity style={{ marginTop:9 }}>
+                <Text style={{ fontSize: 19, textAlign: 'right', color:'#EEEEEE'}}>Lupa Password?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{marginTop: 40, backgroundColor: '#FFBF57', padding: 15, borderRadius: 5}}>
+                <Text style={{color: '#EEEEEE', fontSize: 20, fontWeight: '600', alignSelf: 'center', textAlign:'center'}}>MASUK</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{marginTop: 20, backgroundColor: '#696969', padding: 15, borderRadius: 5, borderColor: '#FFBF57', borderWidth: 1}}>
+                <Text style={{color: '#EEEEEE', fontSize: 20, fontWeight: '600', alignSelf: 'center', textAlign:'center'}}>Belum punya akun? DAFTAR</Text>
+            </TouchableOpacity>
+        </View >
     )
 }
 
 const Login = () => {
     return (
         <View style={styles.container}>
-            <Logo />
+            <Logo/>
+            <FormLogin />
         </View>
     )
 }
@@ -38,8 +67,13 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'center',
         alignContent: 'center',
-        backgroundColor: 'red',
-        alignItems: 'center'
+        alignItems: 'center',
+        marginTop: 130,
+    },
+    FormLogin: {
+        width: '90%',
+        alignSelf: 'center',
+        marginTop: 90,
     }
 })
 export default Login
