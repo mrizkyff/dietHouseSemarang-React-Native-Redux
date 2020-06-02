1f40143d1ecea1de4bebf95f69b63041579cbf91
mrizkyff <muhamadrizkyff@gmail.com>
2020-06-03 05:04:21 +0700

    nyicil login v2

--- screens/login/index.js
+++ screens/login/index.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, Image } from 'react-native'
 
-export class Login extends Component {
-    render() {
-        return (
-            <View>
-                <Text> halaman login v2 </Text>
+// KOMPONEN
+// logo
+const Logo = () => {
+    return (
+        <View style={styles.viewLogo}>
+            <Image source={require('./logo_dhs.png')} style={styles.pictLogo} />
+            <View style={{flexDirection: 'column'}}>
+                <Text style={{ marginLeft: 10, fontSize: 25, color: 'white' }}>Diet House Semarang</Text>
+                <Text style={{ marginLeft: 10, fontSize: 15, color: 'white' }}>Artisan Food</Text>
             </View>
-        )
-    }
+        </View>
+    )
 }
 
+const Login = () => {
+    return (
+        <View style={styles.container}>
+            <Logo />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#696969',
+        flex: 1,
+
+    },
+    pictLogo: {
+        width: 100,
+        height: 100,
+        resizeMode: 'contain',
+    },
+    viewLogo: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignContent: 'center',
+        backgroundColor: 'red',
+        alignItems: 'center'
+    }
+})
 export default Login
