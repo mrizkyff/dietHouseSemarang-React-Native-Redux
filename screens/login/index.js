6076d6b94938f0b3ab2d10849b4daa4ae517d557
mrizkyff <muhamadrizkyff@gmail.com>
2020-06-03 07:14:08 +0700

    form login sudah bisa nyambung dengan rest api

--- screens/login/index.js
+++ screens/login/index.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { Text, View, StyleSheet, Image } from 'react-native'
 import { Input, Item, Icon, Button } from "native-base"
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import axios from 'axios'
 
 // KOMPONEN
 // logo
@@ -19,26 +20,67 @@ const Logo = () => {
 
 // form
 const FormLogin = () => {
+
+    // initial state
+    const initialState = {
+        username: '',
+        password: '',
+    }
+
+    // membuat state dengan usestate
+    const [user, setuser] = useState(initialState)
+
+    // fungsi untuk login
+    const loginUser = (user) => {
+        axios
+            .post('http://192.168.8.104/restApi-dietHouseSemarang/api/login', {
+                username: user.username,
+                password: user.password,
+            })
+            .then(function (response) {
+                // handle success
+                // alert(JSON.stringify(response.data));
+                alert('login berhasil!');
+                // setdata(response.data.data)
+                // console.log(JSON.stringify(response.data))
+            })
+            .catch(function (error) {
+                // handle error
+                alert(error.message);
+            })
+            .finally(function () {
+                // always executed
+                // alert('Finally called');
+                // alert(data);
+                // console.log(data);
+                setuser(initialState);
+
+                // getDataUsingSimpleGetCall();
+            });
+        // alert('halo'+user.username)
+    };
+
     return (
         <View style={styles.FormLogin}>
             {/* <Text>Form Login</Text> */}
             <Item regular style={{ borderColor: '#696969', backgroundColor: '#808080', height: 50 }}>
-                <Input placeholder='Username' placeholderTextColor='#EEEEEE' style={{ color: '#EEEEEE', fontSize: 20 }} />
+                <Input placeholder='Username' placeholderTextColor='#EEEEEE' style={{ color: '#EEEEEE', fontSize: 20 }} onChangeText={(text) => setuser({ ...user, username: text })} />
                 <Icon type="FontAwesome" name="envelope" style={{ color: '#EEEEEE', fontSize: 20 }} />
             </Item>
             <Item regular style={{ borderColor: '#696969', backgroundColor: '#808080', height: 50, marginTop: 20 }}>
-                <Input placeholder='Password' secureTextEntry={true} placeholderTextColor='#EEEEEE' style={{ color: '#EEEEEE', fontSize: 20 }} />
+                <Input placeholder='Password' secureTextEntry={true} placeholderTextColor='#EEEEEE' style={{ color: '#EEEEEE', fontSize: 20 }} onChangeText={(text) => setuser({ ...user, password: text })} />
                 <Icon type="FontAwesome" name="lock" style={{ color: '#EEEEEE', fontSize: 20 }} />
             </Item>
-            <TouchableOpacity style={{ marginTop:9 }}>
-                <Text style={{ fontSize: 19, textAlign: 'right', color:'#EEEEEE'}}>Lupa Password?</Text>
+            <TouchableOpacity style={{ marginTop: 9 }}>
+                <Text style={{ fontSize: 19, textAlign: 'right', color: '#EEEEEE' }}>Lupa Password?</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={{marginTop: 40, backgroundColor: '#FFBF57', padding: 15, borderRadius: 5}}>
-                <Text style={{color: '#EEEEEE', fontSize: 20, fontWeight: '600', alignSelf: 'center', textAlign:'center'}}>MASUK</Text>
+            <TouchableOpacity style={{ marginTop: 40, backgroundColor: '#FFBF57', padding: 15, borderRadius: 5 }} onPress={() => loginUser(user)}>
+                <Text style={{ color: '#EEEEEE', fontSize: 20, fontWeight: '600', alignSelf: 'center', textAlign: 'center' }}>MASUK</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={{marginTop: 20, backgroundColor: '#696969', padding: 15, borderRadius: 5, borderColor: '#FFBF57', borderWidth: 1}}>
-                <Text style={{color: '#EEEEEE', fontSize: 20, fontWeight: '600', alignSelf: 'center', textAlign:'center'}}>Belum punya akun? DAFTAR</Text>
+            <TouchableOpacity style={{ marginTop: 20, backgroundColor: '#696969', padding: 15, borderRadius: 5, borderColor: '#FFBF57', borderWidth: 1 }}>
+                <Text style={{ color: '#EEEEEE', fontSize: 20, fontWeight: '600', alignSelf: 'center', textAlign: 'center' }}>Belum punya akun? DAFTAR</Text>
             </TouchableOpacity>
+            <Text>{'inputan user: ' + user.username + user.password}</Text>
         </View >
     )
 }
@@ -46,7 +88,7 @@ const FormLogin = () => {
 const Login = () => {
     return (
         <View style={styles.container}>
-            <Logo/>
+            <Logo />
             <FormLogin />
         </View>
     )
